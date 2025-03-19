de9c9ab22d7c2dbba00e1e5988b89bf7a072d3fe
snowie0 <rnurkzmz11@gmail.com>
2025-03-19 08:16:07 -0400

    fixed the database bug
    manually create a table for users on the workbench for the easiest fix later it continues to add the users on it

--- secure_web/login.php
+++ secure_web/login.php
@@ -4,25 +4,27 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") { 
     // save the username + password from the form on index.php
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING); // Sanitize input to prevent unwanted characters
+    $username = filter_input(INPUT_POST, 'username', filter: FILTER_SANITIZE_SPECIAL_CHARS); // Sanitize input to prevent unwanted characters
     $password = $_POST['password']; // Password doesn't need to be sanitized, as it will be hashed later
 
     // check if username already in db 
-    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt = $conn->prepare(query: "SELECT password FROM users WHERE username = ?"); # ADD PASW HERE AS WELL
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $stmt->store_result();
+    #$stmt->bind_result($password);
+
 
     if ($stmt->num_rows > 0) {
         // user exists
-        $stmt->bind_result($passw_in_db);
+        $stmt->bind_result( $passw_in_db);
         $stmt->fetch();
         
         // password verify is the function to check the hash if it matches the string
         // we are checking if the password on db == password entered
-        if (password_verify($password, $passw_in_db)) {
+        if (password_verify( $password, $passw_in_db)) {
             $_SESSION['username'] = $username;  // when password matches correctly
-            header("Location: useronly.php");  // if so we go to useronly file
+            header(header: "Location: useronly.php");  // if so we go to useronly file
             exit();
         } else {
             echo "<br> password not correct !";
@@ -39,7 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // mysqli for INSERTING values in db 
         $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-        $stmt->bind_param("ss", $username, $pasw_hashed);
+        $stmt->bind_param( "ss", $username, $pasw_hashed);
 
         if ($stmt->execute()) {
             $_SESSION['username'] = $username;
